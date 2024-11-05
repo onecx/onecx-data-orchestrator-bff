5419c4002b97b807c1cccc77f21abc24ed7d94cb
JordenReuter <149687553+JordenReuter@users.noreply.github.com>
2024-11-05 11:19:28 +0100

    Feature/initial setup (#2)
    * feat: initial setup
    
    * feat: initial impl

---
+++ src/test/java/org/tkit/onecx/data/orchestrator/bff/rs/CrdRestControllerPermissionTest.java
@@ -0,0 +1,91 @@
+package org.tkit.onecx.data.orchestrator.bff.rs;
+
+import static io.restassured.RestAssured.given;
+import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
+import static org.jboss.resteasy.reactive.RestResponse.Status.OK;
+
+import jakarta.inject.Inject;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.tkit.onecx.data.orchestrator.bff.rs.controllers.CrdRestController;
+
+import gen.org.tkit.onecx.data.orchestrator.bff.rs.internal.model.CrdResponseDTO;
+import gen.org.tkit.onecx.data.orchestrator.bff.rs.internal.model.EditResourceRequestDTO;
+import io.fabric8.kubernetes.api.model.apiextensions.v1.CustomResourceDefinition;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.kubernetes.client.WithKubernetesTestServer;
+
+@WithKubernetesTestServer
+@QuarkusTest
+@TestHTTPEndpoint(CrdRestController.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class CrdRestControllerPermissionTest extends AbstractTest {
+
+    @Inject
+    KubernetesClient client;
+
+    @BeforeAll
+    public void before() {
+        // Creating a custom resource from yaml
+        CustomResourceDefinition aCustomResourceDefinition = client.apiextensions().v1().customResourceDefinitions()
+                .load(CrdRestControllerPermissionTest.class.getResourceAsStream("/mocks/permissionDefinition.yml")).item();
+        client.apiextensions().v1().customResourceDefinitions().resource(aCustomResourceDefinition).create();
+        client.resource(CrdRestControllerPermissionTest.class.getResourceAsStream("/mocks/permissionMock.yml")).create();
+    }
+
+    @Test
+    public void testInteractionWithAPIServer() {
+        var response = given()
+                .when()
+                .contentType(APPLICATION_JSON)
+                .auth().oauth2(keycloakClient.getAccessToken(ADMIN))
+                .header(APM_HEADER_PARAM, ADMIN)
+                .get()
+                .then()
+                .contentType(APPLICATION_JSON)
+                .statusCode(OK.getStatusCode())
+                .extract().as(CrdResponseDTO.class);
+        Assertions.assertNotNull(response);
+    }
+
+    @Test
+    public void testEditResource() {
+        var response = given()
+                .when()
+                .auth().oauth2(keycloakClient.getAccessToken(ADMIN))
+                .header(APM_HEADER_PARAM, ADMIN)
+                .get()
+                .then()
+                .contentType(APPLICATION_JSON)
+                .statusCode(OK.getStatusCode())
+                .extract().as(CrdResponseDTO.class);
+        Assertions.assertNotNull(response);
+        var editedData = response.getCrdPermissions().get(0);
+        editedData.getSpec().setAppId("EditedAppId");
+        EditResourceRequestDTO editResourceRequestDTO = new EditResourceRequestDTO();
+        editResourceRequestDTO.setCrdPermission(editedData);
+
+        given().when()
+                .auth().oauth2(keycloakClient.getAccessToken(ADMIN))
+                .header(APM_HEADER_PARAM, ADMIN)
+                .body(editResourceRequestDTO).contentType(APPLICATION_JSON)
+                .post().then().statusCode(OK.getStatusCode());
+
+        response = given()
+                .when()
+                .auth().oauth2(keycloakClient.getAccessToken(ADMIN))
+                .header(APM_HEADER_PARAM, ADMIN)
+                .get()
+                .then()
+                .contentType(APPLICATION_JSON)
+                .statusCode(OK.getStatusCode())
+                .extract().as(CrdResponseDTO.class);
+        Assertions.assertNotNull(response);
+
+    }
+}
---
+++ src/test/java/org/tkit/onecx/data/orchestrator/bff/rs/CrdRestControllerSlotTest.java
@@ -0,0 +1,99 @@
+package org.tkit.onecx.data.orchestrator.bff.rs;
+
+import static io.restassured.RestAssured.given;
+import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
+import static org.jboss.resteasy.reactive.RestResponse.Status.OK;
+
+import java.io.IOException;
+
+import jakarta.inject.Inject;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.tkit.onecx.data.orchestrator.bff.rs.controllers.CrdRestController;
+
+import gen.org.tkit.onecx.data.orchestrator.bff.rs.internal.model.CrdResponseDTO;
+import gen.org.tkit.onecx.data.orchestrator.bff.rs.internal.model.EditResourceRequestDTO;
+import io.fabric8.kubernetes.api.model.apiextensions.v1.CustomResourceDefinition;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.server.mock.KubernetesServer;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.kubernetes.client.KubernetesTestServer;
+import io.quarkus.test.kubernetes.client.WithKubernetesTestServer;
+
+@WithKubernetesTestServer
+@QuarkusTest
+@TestHTTPEndpoint(CrdRestController.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class CrdRestControllerSlotTest extends AbstractTest {
+
+    @KubernetesTestServer
+    KubernetesServer mockServer;
+    @Inject
+    KubernetesClient client;
+
+    @BeforeAll
+    public void before() throws IOException {
+        // Creating a custom resource from yaml
+        CustomResourceDefinition aCustomResourceDefinition = client.apiextensions().v1().customResourceDefinitions()
+                .load(CrdRestControllerSlotTest.class.getResourceAsStream("/mocks/slotDefinition.yml")).item();
+        client.apiextensions().v1().customResourceDefinitions().resource(aCustomResourceDefinition).create();
+        client.resource(CrdRestControllerSlotTest.class.getResourceAsStream("/mocks/slotMock.yml")).create();
+    }
+
+    @Test
+    public void testInteractionWithAPIServer() {
+
+        var response = given()
+                .when()
+                .contentType(APPLICATION_JSON)
+                .auth().oauth2(keycloakClient.getAccessToken(ADMIN))
+                .header(APM_HEADER_PARAM, ADMIN)
+                .get()
+                .then()
+                .contentType(APPLICATION_JSON)
+                .statusCode(OK.getStatusCode())
+                .extract().as(CrdResponseDTO.class);
+        Assertions.assertNotNull(response);
+    }
+
+    @Test
+    public void testEditResource() {
+
+        var response = given()
+                .when()
+                .auth().oauth2(keycloakClient.getAccessToken(ADMIN))
+                .header(APM_HEADER_PARAM, ADMIN)
+                .get()
+                .then()
+                .contentType(APPLICATION_JSON)
+                .statusCode(OK.getStatusCode())
+                .extract().as(CrdResponseDTO.class);
+        Assertions.assertNotNull(response);
+        var editedData = response.getCrdSlots().get(0);
+        editedData.getSpec().setAppId("EditedAppId");
+        EditResourceRequestDTO editResourceRequestDTO = new EditResourceRequestDTO();
+        editResourceRequestDTO.setCrdSlot(editedData);
+
+        given().when()
+                .auth().oauth2(keycloakClient.getAccessToken(ADMIN))
+                .header(APM_HEADER_PARAM, ADMIN)
+                .body(editResourceRequestDTO).contentType(APPLICATION_JSON)
+                .post().then().statusCode(OK.getStatusCode());
+
+        response = given()
+                .when()
+                .auth().oauth2(keycloakClient.getAccessToken(ADMIN))
+                .header(APM_HEADER_PARAM, ADMIN)
+                .get()
+                .then()
+                .contentType(APPLICATION_JSON)
+                .statusCode(OK.getStatusCode())
+                .extract().as(CrdResponseDTO.class);
+        Assertions.assertNotNull(response);
+
+    }
+}

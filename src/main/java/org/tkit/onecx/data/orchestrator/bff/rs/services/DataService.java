6465bdc93712df150259ca957a1850688f339e3e
Jorden_Reuter <149687553+JordenReuter@users.noreply.github.com>
2025-05-12 10:39:11 +0200

    fix: code smell

--- src/main/java/org/tkit/onecx/data/orchestrator/bff/rs/services/DataService.java
+++ src/main/java/org/tkit/onecx/data/orchestrator/bff/rs/services/DataService.java
@@ -9,7 +9,6 @@ import jakarta.inject.Inject;
 import org.tkit.onecx.data.orchestrator.bff.rs.configs.DataOrchestratorConfig;
 
 import gen.org.tkit.onecx.data.orchestrator.bff.rs.internal.model.ContextKindDTO;
-import gen.org.tkit.onecx.data.orchestrator.bff.rs.internal.model.GetContextKindsResponseDTO;
 
 @ApplicationScoped
 public class DataService {
@@ -18,7 +17,6 @@ public class DataService {
     DataOrchestratorConfig config;
 
     public Set<ContextKindDTO> getActiveTypes() {
-        GetContextKindsResponseDTO responseDTO = new GetContextKindsResponseDTO();
         return config.types().values().stream().filter(DataOrchestratorConfig.TypeConfig::enabled)
                 .map(typeConfig -> ContextKindDTO.fromString(typeConfig.value())).collect(Collectors.toSet());
     }
